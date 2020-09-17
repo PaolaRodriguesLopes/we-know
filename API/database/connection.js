7eade017d23b88aff008d7f959a6a692f0385868
Paola Rodrigues <paolarodrigues158@gmail.com>
2020-09-16 22:13:42 -0300

    Adicionado CRUD categoria, corrigido alguns erros em curso e adicionado nova descricao no README

--- API/database/connection.js
+++ API/database/connection.js
@@ -3,7 +3,7 @@ var knex = require('knex')({
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : '',
+      password : 'root',
       database : 'weknow'
     }
   });
