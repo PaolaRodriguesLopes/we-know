6221f53f19645759dfa43aa6b014cff4d7ea8621
Fernando Dias <Fernando.Dias@flex.com>
2020-09-16 21:17:33 -0300

    config connection fernando

--- API/database/connection.js
+++ API/database/connection.js
@@ -3,7 +3,8 @@ var knex = require('knex')({
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : '',
+      //password : 'root',
+      password: '',
       database : 'weknow'
     }
   });
