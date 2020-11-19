5aef8e2c53e4f62f3e68e911a2589df23d1a7b16
PaolaRodriguesLopes <paolarodrigues158@gmail.com>
2020-11-19 19:26:17 -0300

    fix menu

--- weKnow/src/App.vue
+++ weKnow/src/App.vue
@@ -10,44 +10,49 @@
             <a href="/"> Home </a>
           </li>
           <li v-if="sessionUser">
-            <a href="#" @click="redirectToArticles()"> 
-              Meus Artigos 
-            </a>
+            <a href="#" @click="redirectToArticles()"> Meus Artigos </a>
           </li>
           <li v-if="sessionUser">
-            <router-link :to="{ name: 'InsertArticle' }"> 
-              Novo Artigo 
+            <router-link :to="{ name: 'InsertArticle' }">
+              Novo Artigo
             </router-link>
           </li>
-          <div v-if="sessionUser != null && (sessionUser.role === 1 || sessionUser.role === 2)">
+          <div
+            v-if="
+              sessionUser != null &&
+              (sessionUser.role === 1 || sessionUser.role === 2)
+            "
+          >
             <li v-for="link of sideBarMenuLinks" :key="link.home">
-              <a :href="buildSideMenuLinkPath (link)" target="_self">
+              <a :href="buildSideMenuLinkPath(link)" target="_self">
                 {{ link.text }}
               </a>
             </li>
           </div>
-          <li v-if="sessionUser.role === 1">
-            <router-link :to="{ name: 'Categories' }"> 
-              Adcionar Categoria 
-            </router-link>
-          </li>
-          <li v-if="sessionUser.role === 1">
-            <router-link :to="{ name: 'Courses' }"> 
-              Adcionar Curso 
-            </router-link>
-          </li>
-          <li v-if="sessionUser.role === 1">
-            <router-link :to="{ name: 'Subjects' }"> 
-              Adcionar Matéria 
-            </router-link>
-          </li>
+          <div v-if="sessionUser">
+            <li v-if="sessionUser.role === 1">
+              <router-link :to="{ name: 'Categories' }">
+                Adcionar Categoria
+              </router-link>
+            </li>
+            <li v-if="sessionUser.role === 1">
+              <router-link :to="{ name: 'Courses' }">
+                Adcionar Curso
+              </router-link>
+            </li>
+            <li v-if="sessionUser.role === 1">
+              <router-link :to="{ name: 'Subjects' }">
+                Adcionar Matéria
+              </router-link>
+            </li>
+          </div>
           <li><a href="#">Ajuda</a></li>
           <hr />
           <li>
             <a href="/"> Últimos Artigos </a>
           </li>
           <li v-for="link of subjectLinks" :key="link.home">
-            <a :href="buildSideMenuLinkPath (link)" target="_self">
+            <a :href="buildSideMenuLinkPath(link)" target="_self">
               {{ link.text }}
             </a>
           </li>
@@ -57,14 +62,22 @@
     <div class="filterBar" v-if="!hideBars">
       <div class="filters">
         <div class="field field1">
-          <input type="text" placeholder="Buscar" v-model="currentSearchValue" />
+          <input
+            type="text"
+            placeholder="Buscar"
+            v-model="currentSearchValue"
+          />
         </div>
 
         <div class="field field2">
           <select v-model="currentCriteria">
-            <option value=""> Escolha um critério </option>
-            <option v-for="criteria of searchCriterias" :key="criteria.value" v-bind:value="criteria.value">
-                {{ criteria.text }}
+            <option value="">Escolha um critério</option>
+            <option
+              v-for="criteria of searchCriterias"
+              :key="criteria.value"
+              v-bind:value="criteria.value"
+            >
+              {{ criteria.text }}
             </option>
           </select>
         </div>
@@ -74,12 +87,13 @@
             FILTRAR
           </button>
         </div>
-
       </div>
 
       <div class="profile" v-if="isAuthenticated">
-        <p> Seja bem-vindo(a), <span class="p-nome"> {{fullName}} </span><br>
-        <a href="#"> Editar Perfil </a> - <a @click="logout" href=""> Sair </a>        
+        <p>
+          Seja bem-vindo(a), <span class="p-nome"> {{ fullName }} </span><br />
+          <a href="#"> Editar Perfil </a> -
+          <a @click="logout" href=""> Sair </a>
         </p>
       </div>
     </div>
@@ -90,104 +104,120 @@
 
 
 <script>
-  import Helpers from './js/others/Helpers';
-  import SubjectServices from './js/services/SubjectServices';
-  export default {
-
-      created() {
-        this.checkIfIsToHideBars();
-        this.storeLoggedUser();
-        this.getSubjects();
-      },
-      data() {
-          return {
-              componentKey: 0,
-              users: [],
-              showModal: false,
-              deleteUserId: -1,
-              isAuthenticated: false,
-              fullName: '',
-              sessionUser: undefined,
-              hideBars: false,
-              searchCriterias: [
-                { value: 'subjects-subject-description', text: 'Matéria' },
-                { value: 'categories-category-description', text: 'Categoria' },
-                { value: 'users-author-name', text: 'Autor' },
-                { value: 'title', text: 'Tema' },
-              ],
-              currentCriteria: '',
-              currentSearchValue: '',
-              subjectLinks: [],
-              sideBarMenuLinks: [
-                { path: 'Articles', value: 1, text: 'Aprovado por mim', criteria: 'author_status', },
-                { path: 'Articles', value: 0, text: 'Aguardando Aprovação', criteria: 'status_article', },
-              ]
-          }
-      },
-      methods: {
-        checkIfIsToHideBars() {
-          const pathname = location.pathname;
-          console.log('pathname', pathname);
-          this.hideBars = (pathname === '/login' || pathname === '/register' || pathname === '/recoverpassword');
+import Helpers from "./js/others/Helpers";
+import SubjectServices from "./js/services/SubjectServices";
+export default {
+  created() {
+    this.checkIfIsToHideBars();
+    this.storeLoggedUser();
+    this.getSubjects();
+  },
+  data() {
+    return {
+      componentKey: 0,
+      users: [],
+      showModal: false,
+      deleteUserId: -1,
+      isAuthenticated: false,
+      fullName: "",
+      sessionUser: undefined,
+      hideBars: false,
+      searchCriterias: [
+        { value: "subjects-subject-description", text: "Matéria" },
+        { value: "categories-category-description", text: "Categoria" },
+        { value: "users-author-name", text: "Autor" },
+        { value: "title", text: "Tema" },
+      ],
+      currentCriteria: "",
+      currentSearchValue: "",
+      subjectLinks: [],
+      sideBarMenuLinks: [
+        {
+          path: "Articles",
+          value: 1,
+          text: "Aprovado por mim",
+          criteria: "author_status",
         },
-        storeLoggedUser() {
-          let sessionUser = Helpers.getSessionUser();
-          if (sessionUser !== null) {
-            this.isAuthenticated = true;
-            this.sessionUser = sessionUser;
-            this.fullName = sessionUser.name;
-          }
-          else {
-            this.isAuthenticated = false;
-            this.sessionUser = undefined;
-            this.fullName = '';
-          }
+        {
+          path: "Articles",
+          value: 0,
+          text: "Aguardando Aprovação",
+          criteria: "status_article",
         },
-        logout(){
-          localStorage.setItem ('token', null);
-          this.$router.replace ('login');
-        },
-        redirectToArticles() {
-          location.href = `Articles?id=${this.sessionUser.id}`;
-        },
-
-        searchInArticles() {
-          location.href = `Articles?value=${this.currentSearchValue}&criteria=${this.currentCriteria}`;
-        },
-
-        buildSideMenuLinkPath(link) {
-          return `${link.path}?value=${link.value !== undefined ? link.value : link.text}&criteria=${link.criteria}`;
-        },
-
-        getSubjects() {
-          SubjectServices.getSubjects().then(response => {
-            this.subjectLinks = [];
-            const subjects = response.data;
-            subjects.forEach(sub => {
-              this.subjectLinks.push({
-                path: '/', text: sub.description, criteria: 'subjects-subject-description'
-              });
-            });
-          }).catch (() => this.subjectLinks = []);
-        },
-      },
-      filters: {
-          processRole: function(value){
-              if(value == 0){
-                  return "Usuário Comum";
-              }else if(value == 1){
-                  return "Administrador"
-              }else if(value == 2){
-                  return "Professor(a)"
-              }
-          }
+      ],
+    };
+  },
+  methods: {
+    checkIfIsToHideBars() {
+      const pathname = location.pathname;
+      console.log("pathname", pathname);
+      this.hideBars =
+        pathname === "/login" ||
+        pathname === "/register" ||
+        pathname === "/recoverpassword";
+    },
+    storeLoggedUser() {
+      let sessionUser = Helpers.getSessionUser();
+      if (sessionUser !== null) {
+        this.isAuthenticated = true;
+        this.sessionUser = sessionUser;
+        this.fullName = sessionUser.name;
+      } else {
+        this.isAuthenticated = false;
+        this.sessionUser = undefined;
+        this.fullName = "";
       }
-  }
+    },
+    logout() {
+      localStorage.setItem("token", null);
+      this.$router.replace("login");
+    },
+    redirectToArticles() {
+      location.href = `Articles?id=${this.sessionUser.id}`;
+    },
+
+    searchInArticles() {
+      location.href = `Articles?value=${this.currentSearchValue}&criteria=${this.currentCriteria}`;
+    },
+
+    buildSideMenuLinkPath(link) {
+      return `${link.path}?value=${
+        link.value !== undefined ? link.value : link.text
+      }&criteria=${link.criteria}`;
+    },
+
+    getSubjects() {
+      SubjectServices.getSubjects()
+        .then((response) => {
+          this.subjectLinks = [];
+          const subjects = response.data;
+          subjects.forEach((sub) => {
+            this.subjectLinks.push({
+              path: "/",
+              text: sub.description,
+              criteria: "subjects-subject-description",
+            });
+          });
+        })
+        .catch(() => (this.subjectLinks = []));
+    },
+  },
+  filters: {
+    processRole: function (value) {
+      if (value == 0) {
+        return "Usuário Comum";
+      } else if (value == 1) {
+        return "Administrador";
+      } else if (value == 2) {
+        return "Professor(a)";
+      }
+    },
+  },
+};
 </script>
 
 
 <style>
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -219,7 +249,7 @@ body,
 .menuDefault {
   width: 20%;
   min-height: 100%;
-  background: #6D6D6D;
+  background: #6d6d6d;
   float: left;
 }
 
@@ -252,7 +282,7 @@ body,
 
 .filterBar {
   width: 80%;
-  background: #E6E6E6;
+  background: #e6e6e6;
   float: left;
   height: 60px;
   display: flex;
